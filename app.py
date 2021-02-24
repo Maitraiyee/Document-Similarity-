4ddf03ab65b507c2a1f7cfbe6280c0a29dcccd28
Maitraiyee Gautam <68839181+Maitraiyee@users.noreply.github.com>
2021-02-23 23:10:40 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,120 @@
+import sys
+
+def read_file(filename):
+    """This function is used to read the text file"""
+    try:
+        with open(filename, "r", errors='ignore') as file:
+            data = file.read()
+        return data
+
+    except IOError:
+        print("Error opening or reading input file: ", filename)
+        sys.exit()
+
+
+stopwords = read_file('stopwords.txt')
+
+
+# step a: function to remove punctuation/apostrophe/convert to lower
+
+def removal(data):
+    """This function converts uppercase to lowercase, replaces apostrophe and removes punctuation"""
+    y = data
+
+    ##step 1: converting to lower case
+    y = y.lower()
+
+    # step 2: initializing punctuations string
+    punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+
+    # step 3: remove_punctuation
+    for val in y:
+        if val in punc:
+            y = y.replace(val, " ")
+
+    return y
+
+
+def process(text):
+    """ This fucntion is for tokenization, removing stop words"""
+
+    x = text.split()
+
+    # step5: filtering out tokens that are not alphabetic
+    x = [c for c in x if c.isalpha()]
+
+    # step6: removing stop words
+    list_stop_words = stopwords.splitlines()
+    x = [w for w in x if not w in list_stop_words]
+
+    return x
+
+
+# Method - Using Vector distance
+
+def freq_count(text):
+    """This function is to form key value pairs in terms of words in document and their respective frequency"""
+    Dict = {}
+
+    for word in text:
+        if word in Dict:
+            Dict[word] = Dict[word] + 1
+        else:
+            Dict[word] = 1
+
+    return Dict
+
+
+def dotproduct(d1, d2):
+    """This function calculates dot product based on frequency of words in two documents. If the key is same in both documnets
+    then function will simply multiply the frequency values """
+    d1 = freq_count(d1)
+    d2 = freq_count(d2)
+    total = 0.0
+    for key in d1:
+        if key in d2:
+            total += (d1[key] * d2[key])
+    return total
+
+
+def percent_similar(D1, D2):
+    """This function calculates  with the help of dot product. Same documents will show 100% similarity"""
+    numerator = dotproduct(D1, D2)
+    denominator = (dotproduct(D1, D1) * dotproduct(D2, D2)) ** (1 / 2)
+    return (numerator / denominator)
+
+
+
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#using POST method
+@app.route('/predict', methods =['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    if request.method == "POST":
+        message1 = request.form["message 1"]
+        message2 = request.form["message 2"]
+        sample_1 = message1
+        sample_2 = message2
+
+        sample_1 = removal(sample_1)
+        sample_2 = removal(sample_2)
+
+        sample_1 = process(sample_1)
+        sample_2 = process(sample_2)
+
+        prediction = percent_similar(sample_1, sample_2)
+        output = round(prediction * 100, 2)
+
+    return render_template('index.html', prediction_text=("The document samples show  " + str(output) + " percent similarity."))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ readme.txt
@@ -0,0 +1,36 @@
+TO RUN PYTHON FILE LOCALY AND CHECK THE RESULT:
+
+1. Run app.py with templates subfolder containing index.html file in the same project folder.
+   Document_similarity -->app.py, templates ->> index.html 
+2. The link to run app locally on web browser is "0.0.0.0:5000" for linux/ubuntu. For windows, type "localhost:5000" on web browser to run the app.
+
+alternative:
+
+1. Run Document_Similarity.py on windows and check the link in console to run the app.
+
+
+
+RUNNING DOCKER IMAGE LOCALLY:
+
+1. Build docker image : docker build -t flaskapp:version1 .
+2. Run the images : docker run -it -p 5000:5000 flaskapp:version1
+3. (Optional) Check if the container is running: sudo docker ps
+3. Open browser and hit http://localhost:5000/
+4. To stop the container: docker kill <container-id>
+
+
+RUNNING DOCKER IMAGE FROM DOCKERHUB:
+
+Pushing the image:
+
+1. Create Docker hub account.
+2. Create a repository in dockerhub.
+3. Add a tag to the local docker image :  docker tag flaskapp:version1 maitraiyee/flaskapp:version1
+4. Push the image to dockerhub :  docker push maitraiyee/flaskapp:version1
+Fetching and Running the image:
+
+1. Pull the image : docker pull maitraiyee/flaskapp:version1
+2. Run the downloaded image : docker run -it -p 5000:5000 maitraiyee/flaskapp:version1
+
+
+## Note : The above instructions are to run the docker on windows, in app.py localhost-0.0.0.0 has been used so as to make the app version compatible with linux; ubuntu. please use "localhost:5000" in the web browser to run the same app on windows.
